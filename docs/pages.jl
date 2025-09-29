27fccab38d96aff8ed6332a76f62b8960e7e34d3
jClugstor <jadonclugston@gmail.com>
2025-09-29 16:52:41 -0400

    add getting_started, update pages

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,9 +1,16 @@
 pages = [
     "Home" => "index.md",
+    "Getting Started" => "getting_started.md",
     "Tutorials" => [
         "User Guide" => "user_tutorial.md",
         "Developer Guide" => "developer_tutorial.md",
-        "Complete Examples" => "tutorial.md"
     ],
-    "API Reference" => "api.md",
+    "Manual" => [
+        "Message Levels" => "manual/manual_message_levels.md",
+        "Verbosity Specifiers" => "manual/manual_verbosity_specifiers.md",
+        "Verbosity Presets" => "manual/manual_presets.md",
+        "@SciMLMessage Macro" => "manual/manual_macro.md",
+        "Conversion Utilities" => "manual/manual_conversion_utilities.md",
+        "Logging Backends" => "manual/manual_backends.md"
+    ]
 ]
