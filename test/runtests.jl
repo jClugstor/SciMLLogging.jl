9fd7089742e105f82ab11e612a018925926c4b14
jClugstor <jadonclugston@gmail.com>
2026-04-23 12:13:51 -0400

    add downstream testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
+using Pkg
 using Test
 using SafeTestsets
 
-@time @safetestset "Basic Tests" include("basics.jl")
-@time @safetestset "Verbosity Specifier Generation Tests" include("generation_test.jl")
-@time @safetestset "Explicit Imports" include("explicit_imports.jl")
+const GROUP = get(ENV, "GROUP", "All")
+
+function activate_downstream_env()
+    Pkg.activate("downstream")
+    Pkg.develop(PackageSpec(path = dirname(@__DIR__)))
+    return Pkg.instantiate()
+end
+
+if GROUP == "Core" || GROUP == "All"
+    @time @safetestset "Basic Tests" include("basics.jl")
+    @time @safetestset "Verbosity Specifier Generation Tests" include("generation_test.jl")
+    @time @safetestset "Explicit Imports" include("explicit_imports.jl")
+    if isempty(VERSION.prerelease)
+        @time @safetestset "JET" include("jet.jl")
+    end
+end
+
+if GROUP == "Downstream"
+    activate_downstream_env()
+    @time @safetestset "Downstream solve JET" include("downstream/solve_jet.jl")
+end
